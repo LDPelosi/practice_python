d34063a06cd7db10a519d7197950944c8f215bad
ldpelosi <ldpelosi@gmail.com>
2021-10-20 20:46:19 -0300

    update the messages

--- exercise_2_extra.py
+++ exercise_2_extra.py
@@ -2,9 +2,9 @@ def run():
     num = int(input("Enter a number: "))
     check = int(input("Enter a second number: "))
     if num % check == 0:
-        print(f'{num} and {check} is divided evenly')
+        print(f'{num} divides evenly by {check}')
     else:
-        print(f'{num} and {check} dont´t divided evenly')
+        print(f'{num} doesn´t diveds evenly by {check} ')
 
 
 if __name__ == '__main__':
