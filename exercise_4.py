e035f2a7d3fbcbc7e98f74b110af493928261af2
ldpelosi <ldpelosi@gmail.com>
2022-04-05 19:55:27 -0300

    exercise 4 upgrade

--- exercise_4.py
+++ exercise_4.py
@@ -1,15 +1,22 @@
 def divisor(n1):
     dividers = []
-    for i in range(1, int(n1 / 2 + 1)):
-        if n1 % i == 0:
-            dividers.append(i)
-    return dividers
+    try:
+        if n1 < 0:
+            raise ValueError('Negative numbers are not valid')
+        for i in range(1, int(n1 / 2 + 1)):
+            if n1 % i == 0:
+                dividers.append(i)
+        return dividers
+    except ValueError as ve:
+        return ve
 
 
 def run():
-    num = int(input('Choose a number: '))
-    dividers = divisor(num)
-    print(dividers)
-
+    try:
+        num = int(input('Choose a number: '))
+        dividers = divisor(num)
+        print(dividers)
+    except ValueError:
+        print('Value is not valid')
 if __name__ == '__main__':
     run()
